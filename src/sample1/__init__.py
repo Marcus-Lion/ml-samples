7371dbb54b518b60b7f439089659e37f429089b4
Marc Deveaux <mdvx@users.noreply.github.com>
2023-10-18 14:51:11 -0400

    0.2.0

--- src/sample1/__init__.py
+++ src/sample1/__init__.py
@@ -1,5 +1,5 @@
-import logging
-import pandas as pd
+import marcuslion as ml
+from marcuslion import *
 
 logger = logging.getLogger('ml_py')
 logger.setLevel(logging.DEBUG)
--- tests/ml_lib_test.py
+++ tests/ml_lib_test.py
@@ -1,8 +1,11 @@
 
-from src.marcuslion.marcuslion import ml_search
+from marcuslion import ml_search, ml_help
+
 
 if __name__ == '__main__':
     try:
+        ml_help()
+
         df = ml_search("bike", "kaggle,usgov")
         print(df.head(3))
         print(df.tail(3))
