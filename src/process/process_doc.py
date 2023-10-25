ea41dd137cd0f0685d1e71c9113ffccbd89e3bc2
Nick Kravitz <nick@marcuslion.com>
2023-10-24 21:56:26 -0400

    process Edgar docs to Mongo

--- src/process/process_doc.py
+++ src/process/process_doc.py
@@ -16,7 +16,7 @@ client = None
 urlArchives = "https://www.sec.gov/Archives/"
 MOZILLA_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 docType = '10-K'
-docDateStart = 20231011
+docDateStart = 20231001
 docDateEnd = 20231019
 
 def connectMongo():
